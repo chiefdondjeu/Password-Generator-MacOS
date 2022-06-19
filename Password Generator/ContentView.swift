85715c4e711b124992bb7fc49c0bb7cc80693ee2
Florent Dondjeu Tschoufack <florentdondjeu@yahoo.com>
2022-06-19 10:58:19 -0400

    Initial Commit

---
+++ Password Generator/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Password Generator
+//
+//  Created by Florent Dondjeu Tschoufack on 6/19/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Password Generator/Password_GeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Password_GeneratorApp.swift
+//  Password Generator
+//
+//  Created by Florent Dondjeu Tschoufack on 6/19/22.
+//
+
+import SwiftUI
+
+@main
+struct Password_GeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
